9cdea0bdd9ebc687a9b2a7033f6e7e444f204f37
Sandhya95Patil <sandhya364@gmail.com>
2024-03-06 00:06:20 +0530

    Second commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Encapsulation
+{
+     public class person
+    {
+        private String Name;
+        private int age;
+
+        public void SetName(String Name,int age)
+
+        {
+            this.Name = Name;
+            this.age = age;
+        }
+        public void GetName()
+        {
+            Console.WriteLine("Name is :- " + this.Name);
+            Console.WriteLine("Age is :-  " + this.age);
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           person p= new person();
+            p.SetName("Nisha",23);
+            p.GetName();
+        }
+    }
+}
