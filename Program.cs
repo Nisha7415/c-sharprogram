d3128c6abd74ec19edefbd23f32bcefc0ede7587
Sandhya95Patil <sandhya364@gmail.com>
2024-03-07 23:09:01 +0530

    methodoveriding commmited

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MethodOverriding
+{
+    class parent
+    {
+        public virtual void print()
+        {
+            Console.WriteLine("This is the method of Parent class.");
+        }
+    }
+    class child : parent 
+    {
+        public override void print()
+        {
+            Console.WriteLine("This is the method of child class.");
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            parent p=new child();
+            p.print();
+        }
+    }
+}
