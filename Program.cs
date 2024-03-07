c514df62348ea861be6d96835ba16f03cbc307c2
Sandhya95Patil <sandhya364@gmail.com>
2024-03-07 23:01:47 +0530

    Interface Commited

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Interface
+{
+    interface inter1
+    {
+        void display();
+    }
+
+    class testClass : inter1
+    {
+        public void display()
+        {
+            Console.WriteLine("This is the method of testclass.");
+        }
+    }
+     public class Program
+     {
+            static void Main(string[] args)
+            {
+                testClass tc = new testClass();
+                tc.display();
+            }
+     }
+}
+
