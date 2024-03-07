03dc4f4ad43f4ff3d84b17d0fce3c649eaa44e71
Sandhya95Patil <sandhya364@gmail.com>
2024-03-07 23:12:53 +0530

    multilevel inheritance commited

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace MultilevelInheritance
+{
+    class BaseClass
+    {
+        public string brand = "Ford";
+        public void show1()
+        {
+            Console.WriteLine("This is a method of Base class..");
+        }
+    }
+
+    class DerivedClass1 : BaseClass
+    {
+        public void show2() 
+        {
+            Console.WriteLine("This is a method of First Derived class..");
+        }
+    }
+    class DerivedClass2 : DerivedClass1
+    {
+        public void show3()
+        {
+            Console.WriteLine("This is a method of Second Derived class..");
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass2 dc = new DerivedClass2();
+            dc.show2();
+            dc.show1();
+            dc.show3();
+        }
+    }
+}
