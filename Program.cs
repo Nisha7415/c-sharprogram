790380a7ff1962990f26f49fbca936c9e14c9d9b
Sandhya95Patil <sandhya364@gmail.com>
2024-03-07 23:20:47 +0530

    single inheritance commited

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SingleInheritance
+{
+    class BaseClass
+    {
+        public string brand = "Ford";  
+        public void show1()             
+        {
+            Console.WriteLine("This is a method of Base class..");
+        }
+    }
+
+    class DerivedClass : BaseClass 
+    {
+        public void show2()
+        {
+            Console.WriteLine("This is a method of Derived class..");
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            DerivedClass dc= new DerivedClass();
+            dc.show2();
+            dc.show1 ();
+        }
+    }
+}
